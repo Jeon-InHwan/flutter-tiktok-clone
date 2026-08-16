050a7d7dfd6d1df01fff4f9fcfcddff74ee55708
Inhwan Jeon <zenstacker@gmail.com>
2026-08-16 10:18:19 +0900

    Implement AnimatedList widget with itemBuilder

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -10,6 +10,17 @@ class ChatScreeen extends StatefulWidget {
 }
 
 class _ChatScreeenState extends State<ChatScreeen> {
+  final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
+
+  final List<int> _items = [];
+
+  void _addItem() {
+    if (_key.currentState != null) {
+      _key.currentState!.insertItem(_items.length);
+      _items.add(_items.length);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,42 +29,50 @@ class _ChatScreeenState extends State<ChatScreeen> {
         title: const Text("Direct messages"),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _addItem,
             icon: const FaIcon(FontAwesomeIcons.plus),
           ),
         ],
       ),
-      body: ListView(
+      body: AnimatedList(
+        key: _key,
         padding: const EdgeInsets.symmetric(vertical: Sizes.size10),
-        children: [
-          ListTile(
-            leading: const CircleAvatar(
-              radius: 25,
-              foregroundImage: NetworkImage(
-                "https://avatars.githubusercontent.com/u/77829187?v=4",
-              ),
-              child: Text("AAA"),
-            ),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                const Text(
-                  "JEON",
-                  style: TextStyle(fontWeight: FontWeight.w600),
-                ),
-                Text(
-                  "9:57 AM",
-                  style: TextStyle(
-                    color: Colors.grey.shade500,
-                    fontSize: Sizes.size12,
+        itemBuilder: (context, index, animation) {
+          return FadeTransition(
+            key: UniqueKey(),
+            opacity: animation,
+            child: SizeTransition(
+              sizeFactor: animation,
+              child: ListTile(
+                leading: const CircleAvatar(
+                  radius: 25,
+                  foregroundImage: NetworkImage(
+                    "https://avatars.githubusercontent.com/u/77829187?v=4",
                   ),
+                  child: Text("AAA"),
                 ),
-              ],
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      "JEON ($index)",
+                      style: const TextStyle(fontWeight: FontWeight.w600),
+                    ),
+                    Text(
+                      "9:57 AM",
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontSize: Sizes.size12,
+                      ),
+                    ),
+                  ],
+                ),
+                subtitle: const Text("We talked about that yesterday"),
+              ),
             ),
-            subtitle: const Text("We talked about that yesterday"),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
